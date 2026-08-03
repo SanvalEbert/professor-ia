975698664b244ad6e4da44297331572ff1d808eb
SanvalEbert <115511942+SanvalEbert@users.noreply.github.com>
2026-08-02 21:50:53 -0300

    fix: direciona fotografia para o arquivo San_10.jpg

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/sanval-ebert.jpg",
+        destination: "/San_10.jpg",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
